229c619863b606f94d08c5b1ddc7a6112d043df1
Paulina Barcos Melchiori <paulinabarcosmelchiori@gmail.com>
2025-07-01 20:35:28 -0300

    arreglamos problema de sessions.router

--- src/controllers/sessions.controller.js
+++ src/controllers/sessions.controller.js
@@ -1,6 +1,7 @@
 import UsersRepository from "../repository/users.repository.js";
 import UsersDAO from "../dao/users.dao.js";
 import SessionsService from "../services/sessions.service.js";
+import UsersDTO from "../dto/users.dto.js";
 
 const sessionsService = new SessionsService(
   new UsersRepository(new UsersDAO())
@@ -26,7 +27,7 @@ const register = async (req, res) => {
     res.send({ status: "success", payload: newUser._id });
   } catch (error) {
     req.logger.error(`Error al registrar usuario: ${error.message}`);
-    res.status(400).send({ status: "error", error: error.message });
+    res.status(500).send({ status: "error", error: error.message });
   }
 };
 
@@ -41,7 +42,6 @@ const login = async (req, res) => {
     }
 
     const token = await sessionsService.login(email, password);
-
     req.logger.info(`Login exitoso del usuario: ${email}`);
     res
       .cookie("coderCookie", token, { maxAge: 3600000 })
@@ -56,10 +56,45 @@ const current = async (req, res) => {
   try {
     const cookie = req.cookies["coderCookie"];
     const user = sessionsService.verifyToken(cookie);
-
     res.send({ status: "success", payload: user });
   } catch (error) {
     req.logger.error(`Error al obtener usuario actual: ${error.message}`);
+    res
+      .status(401)
+      .send({ status: "error", error: "Token inválido o expirado" });
+  }
+};
+
+const unprotectedLogin = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      req.logger.warn("Unprotected login con campos incompletos");
+      return res
+        .status(400)
+        .send({ status: "error", error: "Valores incompletos" });
+    }
+
+    const token = await sessionsService.login(email, password);
+    req.logger.info(`Usuario logueado sin protección: ${email}`);
+    res
+      .cookie("unprotectedCookie", token, { maxAge: 3600000 })
+      .send({ status: "success", message: "Unprotected Logged in" });
+  } catch (error) {
+    req.logger.error(`Error en unprotected login: ${error.message}`);
+    res.status(400).send({ status: "error", error: error.message });
+  }
+};
+
+const unprotectedCurrent = async (req, res) => {
+  try {
+    const cookie = req.cookies["unprotectedCookie"];
+    const user = sessionsService.verifyToken(cookie);
+    res.send({ status: "success", payload: user });
+  } catch (error) {
+    req.logger.error(
+      `Error al verificar unprotected current: ${error.message}`
+    );
     res
       .status(401)
       .send({ status: "error", error: "Token inválido o expirado" });
@@ -70,4 +105,6 @@ export default {
   register,
   login,
   current,
+  unprotectedLogin,
+  unprotectedCurrent,
 };
